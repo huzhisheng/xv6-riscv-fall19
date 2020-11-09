1c4ca51916d0f4087e664cb7e775c06e21045993
huzhisheng <1216265845@qq.com>
2020-11-09 20:36:17 +0800

    借鉴proc.c中的代码, 完成Uthread: switching between threads

--- user/uthread.c
+++ user/uthread.c
@@ -9,10 +9,28 @@
 
 #define STACK_SIZE  8192
 #define MAX_THREAD  4
+struct context {
+  uint64 ra;
+  uint64 sp;
 
+  // callee-saved
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+};
 struct thread {
   char       stack[STACK_SIZE]; /* the thread's stack */
   int        state;             /* FREE, RUNNING, RUNNABLE */
+  struct context context;
 };
 struct thread all_thread[MAX_THREAD];
 struct thread *current_thread;
@@ -21,7 +39,7 @@ extern void thread_switch(uint64, uint64);
 void 
 thread_init(void)
 {
-  // main() is thread 0, which will make the first invocation to
+  // main() is thread 0, which will make the first invocation(调用) to
   // thread_schedule().  it needs a stack so that the first thread_switch() can
   // save thread 0's state.  thread_schedule() won't run the main thread ever
   // again, because its state is set to RUNNING, and thread_schedule() selects
@@ -61,6 +79,7 @@ thread_schedule(void)
      * Invoke thread_switch to switch from t to next_thread:
      * thread_switch(??, ??);
      */
+     thread_switch((uint64)(&t->context), (uint64)(&next_thread->context));
   } else
     next_thread = 0;
 }
@@ -75,6 +94,10 @@ thread_create(void (*func)())
   }
   t->state = RUNNABLE;
   // YOUR CODE HERE
+  //保存31个寄存器在31*8 = 248字节
+  memset(&t->context,0,sizeof(t->context));  //借鉴了proc.c中代码
+  t->context.ra = (uint64)func;
+  t->context.sp = ((uint64)(t->stack)) + STACK_SIZE;
 }
 
 void 
